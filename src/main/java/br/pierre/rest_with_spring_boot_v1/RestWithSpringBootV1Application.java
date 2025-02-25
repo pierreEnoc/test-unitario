72be8019a62b235d702aa04684ad66d31627ab27
Pierre enoc <pierre_enoc@yahoo.fr>
2025-02-25 20:27:54 -0300

    backend project created

---
+++ src/main/java/br/pierre/rest_with_spring_boot_v1/RestWithSpringBootV1Application.java
@@ -0,0 +1,13 @@
+package br.pierre.rest_with_spring_boot_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/pierre/rest_with_spring_boot_v1/RestWithSpringBootV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pierre.rest_with_spring_boot_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
